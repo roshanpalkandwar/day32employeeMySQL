89a8b9a14fe504705916386b708c0eeb37ce8711
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-10-12 22:02:11 +0530

    [Roshan]Normalazation And Er Digram

--- Day32EmployeeSQL.sql
+++ Day32EmployeeSQL.sql
@@ -82,20 +82,16 @@ from employee_payroll1
 
 
 
-create table Taxable
+	create table Taxable
 	(
 		tax_id int not null default ('1') primary key,
 		Deductions int not null,
 		Taxable_Pay int not null ,
 		tax int not null
 		)
-		drop table Taxable
+
 select*
 from Taxable
-
-insert into Taxable(tax_id,Deductions,Taxable_Pay,tax) values(453,4500,(40000-4500),0.05*(40000-4500));
-
-
 
 
 	create table PayrollDetails
@@ -105,12 +101,6 @@ insert into Taxable(tax_id,Deductions,Taxable_Pay,tax) values(453,4500,(40000-45
 	NetPay int not null,
 	tax_id int not  null default ('1') foreign key references Taxable(tax_id)
 	)
-	drop table  PayrollDetails
-
-insert into PayrollDetails(Payroll_id,Basic_Pay,NetPay,tax_id) values(21,40000,(40000-4500-0.05*35500),453);
-
-
-
 
 select*
 from PayrollDetails
@@ -119,22 +109,21 @@ from PayrollDetails
 
 	create table Contact_Details
 	(
-		name1 varchar(200) not null,
+		name varchar(20) not null,
 		phoneNumber varchar(12) not null,
 		Address varchar(max) not null,
 		Payroll_id int not null default ('01') foreign key references PayrollDetails(Payroll_id)
 			
 	)
-	drop table Contact_Details
-	insert into Contact_Details(name1,phoneNumber,Address,Payroll_id) values('bill','9834734334','pawansutnagar',21)
 
 select*
 from Contact_Details
-select*
-from PayrollDetails
-select*
-from Taxable
 
 
-SELECT Basic_Pay FROM PayrollDetails
-WHERE name1 = 'bill'
+
+
+
+
+	
+
+
